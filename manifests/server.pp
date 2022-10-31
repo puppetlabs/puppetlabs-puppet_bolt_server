85b02f2afbbfa0fcfbd14e34124861b7f96d2ed9
Ed Mckain <mckain.default@gmail.com>
2022-10-31 17:14:44 +0100

    splits the package source

--- manifests/server.pp
+++ manifests/server.pp
@@ -3,11 +3,16 @@
 # This class installs and configures Bolt
 #
 class puppet_bolt_server::server (
-  Optional[String] $package_source = 'https://yum.puppet.com/puppet-tools-release-el-8.noarch.rpm'
+  # Optional[String] $package_source = 'https://yum.puppet.com/puppet-tools-release-el-8.noarch.rpm'
 ) {
+  package { 'puppet-tools-release':
+    ensure => present,
+    source => "https://yum.puppet.com/puppet-tools-release-el-${facts['os']['release']['major']}.noarch.rpm",
+  }
+
   package { 'puppet-bolt':
-    name   => 'puppet-bolt',
-    ensure => installed,
-    source => $package_source,
+    name    => 'puppet-bolt',
+    ensure  => present,
+    require => Package['puppet-tools-release'],
   }
 }
