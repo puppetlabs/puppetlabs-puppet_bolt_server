24ec180182ad5a5e9155342df5d486e26fbaa47b
Ed Mckain <mckain.default@gmail.com>
2022-11-01 17:23:00 +0100

    fixes the specs

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -2,10 +2,14 @@
 
 require 'spec_helper'
 
-describe 'puppet_bolt_server::server' do
+describe 'puppet_bolt_server' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { {
+        puppet_token: sensitive('secret_token_here')
+       }
+      }
 
       it { is_expected.to compile }
     end
