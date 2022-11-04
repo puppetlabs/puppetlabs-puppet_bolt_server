7953738d0a3991649b03847b629c17e85a7058d5
Ed Mckain <mckain.default@gmail.com>
2022-11-04 19:49:33 +0100

    removes quotes to fix JSON

--- tasks/bolt_server_status.sh
+++ tasks/bolt_server_status.sh
@@ -3,4 +3,4 @@
 number_bolt_processes_running=$(ps -ef | grep "ruby.*bolt plan run" | grep -v grep | wc -l);
 free_memory=$(free -m -w | xargs | awk '{print"[{\"type\": \"RAM\",\""$1"\": "$9",\""$2"\": "$10",\""$3"\": "$11",\""$4"\": "$12",\""$5"\": "$13",\""$6"\": "$14",\""$7"\": "$15"},{\"type\": \"Swap\",\""$1"\": "$17",\""$2"\": "$18",\""$3"\": "$19"}]"}')
 
-printf '{"number_bolt_processes":"%s","free_memory":"%s"}' "$number_bolt_processes_running" "$free_memory"
+printf '{"number_bolt_processes":"%s","free_memory":%s}' "$number_bolt_processes_running" "$free_memory"
