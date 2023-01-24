c0198f808916b425fbebc9aa0eb896015ec565e7
Dimitri Tischenko <dimitri@puppet.com>
2023-01-24 14:25:11 +0100

    remove bundler cache

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,7 +1,11 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
+# frozen_string_literal: true
 
-RSpec.configure do |c|
-  c.after(:suite) do
-    RSpec::Puppet::Coverage.report!
-  end
-end
+# Load the BoltSpec library
+require 'bolt_spec/plans'
+
+# Configure Puppet and Bolt for testing
+BoltSpec::Plans.init
+
+# This environment variable can be read by Ruby Bolt tasks to prevent unwanted
+# auto-execution, enabling easy unit testing.
+ENV['RSPEC_UNIT_TEST_MODE'] ||= 'TRUE'
