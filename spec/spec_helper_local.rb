0f5ff4d0274606f99158954267a6b38ba2fc087c
Dimitri Tischenko <dimitri@puppet.com>
2023-01-30 23:26:41 +0100

    add coverage

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -9,3 +9,11 @@ BoltSpec::Plans.init
 # This environment variable can be read by Ruby Bolt tasks to prevent unwanted
 # auto-execution, enabling easy unit testing.
 ENV['RSPEC_UNIT_TEST_MODE'] ||= 'TRUE'
+
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |c|
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+end
