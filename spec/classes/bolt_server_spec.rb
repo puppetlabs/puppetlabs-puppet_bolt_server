8e30a16c90f90d5216b0742228a3adc329a73078
Dimitri Tischenko <dimitri@puppet.com>
2023-01-30 22:32:45 +0100

    small updates

---
+++ spec/classes/bolt_server_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'puppet_bolt_server' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) { { puppet_token: sensitive('secret_token_here') } }
+
+      it { is_expected.to compile }
+      it { is_expected.to have_resource_count(11) }
+
+      context 'installs bolt' do
+        it { is_expected.to contain_package('puppet-tools-release') }
+        it { is_expected.to contain_package('puppet-bolt') }
+      end
+
+      context 'configures bolt' do
+        it { is_expected.to contain_file('/root/.puppetlabs') }
+        it { is_expected.to contain_file('/root/.puppetlabs/bolt') }
+        it { is_expected.to contain_file('/root/.puppetlabs/etc') }
+        it { is_expected.to contain_file('/root/.puppetlabs/etc/bolt') }
+        it { is_expected.to contain_file('puppet-token') }
+        it { is_expected.to contain_file('/root/.puppetlabs/bolt/bolt-project.yaml') }
+        it { is_expected.to contain_file('/root/.puppetlabs/etc/bolt/bolt-defaults.yaml') }
+        it { is_expected.to contain_file('/var/log/puppetlabs/bolt-server/bolt-server.log') }
+      end
+    end
+  end
+end
