a8bc7ed49fb3f4db479abb592047333dc107a347
Dimitri Tischenko <dimitri@puppet.com>
2023-01-31 21:42:22 +0100

    pdk update

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -16,9 +16,3 @@ require 'bolt_spec/plans'
 ENV['RSPEC_UNIT_TEST_MODE'] ||= 'TRUE'
 
 require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |c|
-  c.after(:suite) do
-    RSpec::Puppet::Coverage.report!
-  end
-end
